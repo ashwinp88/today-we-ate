26bfdea1a8546c811accc9c65a1f0d0f722c3a35
Ashwin Purushotaman <ashwin.p88@gmail.com>
2026-01-14 21:18:56 -0500

    chore: remove unused landing+jumbotron assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
 
   helper_method :current_user, :user_signed_in?
-  helper_method :session_background_asset, :session_jumbotron_asset
+  helper_method :session_background_asset
 
   private
 
@@ -29,7 +29,6 @@ class ApplicationController < ActionController::Base
   end
 
   before_action :pick_background_for_session
-  before_action :pick_jumbotron_for_session
 
   def pick_background_for_session
     return if session[:bg_image].present?
@@ -44,35 +43,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def session_jumbotron_asset
-    session[:jumbo_image]
-  end
-
-  def pick_jumbotron_for_session
-    landing_dir = Rails.root.join("app", "assets", "images", "landing")
-
-    # Always prefer the landing set and reselect if not present or not a landing image
-    current = session[:jumbo_image]
-    if current.present?
-      fs_path = Rails.root.join("app", "assets", "images", current)
-      unless current.start_with?("landing/") && File.exist?(fs_path)
-        session.delete(:jumbo_image)
-        current = nil
-      end
-    end
-
-    return if current.present?
-
-    if Dir.exist?(landing_dir)
-      files = Dir.children(landing_dir).grep(/\.(png|jpe?g|webp|avif)\z/i)
-      if files.any?
-        session[:jumbo_image] = File.join("landing", files.sample)
-        Rails.logger.info("[HERO] Selected #{session[:jumbo_image]}")
-      else
-        Rails.logger.info("[HERO] No landing images found in #{landing_dir}")
-      end
-    else
-      Rails.logger.info("[HERO] Landing images directory missing: #{landing_dir}")
-    end
-  end
 end
